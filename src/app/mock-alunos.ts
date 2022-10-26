274001d9b56e463001a4f1861649160ed750f23e
Joyce Santiago <joyce_fsantiago@outlook.com>
2022-10-25 21:34:45 -0300

    first commit

---
+++ src/app/mock-alunos.ts
@@ -0,0 +1,14 @@
+import { Aluno } from './aluno';
+
+export const ALUNOS: Aluno[] = [
+    {id: 1, nome: 'Beatriz Barros', idade: 15, cursando:'Fundamental'},
+    {id: 2, nome: 'Ana Luiza Rocha', idade: 12, cursando:'Fundamental'},
+    {id: 3, nome: 'Antônio Viana', idade: 13, cursando:'Fundamental'},
+    {id: 4, nome: 'Theo Duarte', idade: 12, cursando:'Fundamental'},
+    {id: 5, nome: 'Melissa Fogaça', idade: 11, cursando:'Fundamental'},
+    {id: 6, nome: 'Gustavo Sales', idade: 16, cursando:'Médio'},
+    {id: 7, nome: 'Rebeca da Cruz', idade: 17, cursando:'Médio'},
+    {id: 8, nome: 'Pedro Miguel Gomes', idade: 17, cursando:'Médio'},
+    {id: 9, nome: 'Maria Fernanda Ramos', idade: 18, cursando:'Médio'},
+    {id: 10, nome: 'Caio Lima', idade: 17, cursando:'Médio'}
+];
